ad67885833391b776c95c50b0ad6a1f3814cbaf7
popstarfreas <popstarfreas2@gmail.com>
2016-08-15 23:04:01 +0100

    Set config to example contents

--- config.js
+++ config.js
@@ -3,29 +3,11 @@
       servers: [{
           listenPort: "3000",
           routingServers: [{
-            name: "main",
+            name: "ExampleServer",
             serverIP: "127.0.0.1",
             serverPort: "7777",
           }]
         },
-
-        {
-          listenPort: "7779",
-          routingServers: [{
-            name: "zombies",
-            serverIP: "142.4.202.106",
-            serverPort: "7002"
-          }]
-        },
-
-        {
-          listenPort: "7776",
-          routingServers: [{
-            name: "pvp",
-            serverIP: "localhost",
-            serverPort: "7777"
-          }]
-        }
       ],
 
       options: {
