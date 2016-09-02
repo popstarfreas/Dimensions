f47ce0a04698bc01dae6412befb9aa6eed4e8eba
Aaren <popstarfreas2@gmail.com>
2016-09-02 19:01:02 +0100

    Add exception catcher due to unknown crash bug

--- index.ts
+++ index.ts
@@ -1,2 +1,10 @@
 import Dimensions from "./dimensions";
+import * as fs from "fs";
+
+process.on('uncaughtException', function(e) {
+   fs.appendFile('../error-log.txt', `${e}\n`, function (err) {
+
+   });
+});
+
 var dimensions = new Dimensions();
