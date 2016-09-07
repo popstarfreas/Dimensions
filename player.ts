85f84c4673debed1c3e5ab4bca69d00e6364fccc
Aaren <popstarfreas2@gmail.com>
2016-09-07 09:34:08 +0100

    Add life and mana properties to player

--- player.ts
+++ player.ts
@@ -3,9 +3,14 @@ class Player {
 	id: number;
 	name: string;
 	inventory: Item[];
+	life: number;
+	mana: number;
+
 	constructor() {
 		this.id = 0;
 		this.name = "";
+		this.life = 100;
+		this.mana = 20;
 
 		// Inventory of Client - only used for SSC -> to Non-SSC switching
 		this.inventory = [];
