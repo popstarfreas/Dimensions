4344f639e1309abb6d98aea32b83b18fb1b03e6f
Lenos <popstarfreas2@gmail.com>
2016-11-16 20:04:10 +0000

    Add more handlers

--- extension.ts
+++ extension.ts
@@ -10,12 +10,29 @@ export interface PacketHandlers {
     server?: ServerPacketHandler;
 }
 
+type ClientErrorHandler = (client: Client, error: Error) => boolean;
+type ServerErrorHandler = (server: TerrariaServer, error: Error) => boolean;
+type ClientDisconnectHandler = (client: Client) => boolean;
+type ServerDisconnectHandler = (server: TerrariaServer) => boolean;
+
 export interface Extension {
     name: string;
     version: string;
     author: string;
+
+    // Packet Handling
     priorPacketHandlers: PacketHandlers,
     postPacketHandlers: PacketHandlers
+
+    // Error Handling
+    clientErrorHandler?: ClientErrorHandler,
+    serverErrorHandler?: ServerErrorHandler
+
+    // Socket close Handling
+    clientDisconnectPreHandler?: ClientDisconnectHandler,
+    clientDisconnectHandler?: ClientDisconnectHandler,
+    serverDisconnectPreHandler?: ServerDisconnectHandler
+    serverDisconnectHandler?: ServerDisconnectHandler
 }
 
 export default Extension;
