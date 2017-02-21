c78b415dcc22a760920bf11cd4dab446ee8d5592
Aaren Cooper <popstarfreas2@gmail.com>
2017-02-21 20:27:33 +0000

    Remove outdated typings

--- typings/index.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="globals/node/index.d.ts" />
-/// <reference path="modules/glob/index.d.ts" />
-/// <reference path="modules/lodash/index.d.ts" />
-/// <reference path="modules/redis/index.d.ts" />
-/// <reference path="modules/uuid/index.d.ts" />
