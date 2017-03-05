d35ec986277aa156cbaf360780f52147ddb59721
Aaren Cooper <popstarfreas2@gmail.com>
2017-03-05 16:49:24 +0000

    Add tests for blacklist

---
+++ spec/dimensions/blacklistspec.ts
@@ -0,0 +1,88 @@
+import BlackList from 'dimensions/blacklist';
+let Mitm = require('mitm');
+
+describe("blacklist", () => {
+    let mitm;
+    let blacklist = new BlackList();
+    
+    beforeEach(() => {
+        mitm = Mitm();
+        blacklist = new BlackList();
+    });
+
+    afterEach(() => {
+        mitm.disable();
+    });
+
+    it("should return false when an ip is not blocked", (done) => {
+        mitm.on("request", (req, res) => {
+            let response = JSON.stringify({
+                status: "success",
+                "host-ip": false
+            });
+            res.statusCode = 200;
+            res.end(response);
+        });
+
+        blacklist.checkIP("127.0.0.1", "").then((isHostIp) => {
+            expect(isHostIp).toBe(false);
+            done();
+        }).catch((e) => {
+            expect(e).toBeNull();
+            done();
+        });
+    });
+
+    it("should return true when an ip not blocked", (done) => {
+        mitm.on("request", (req, res) => {
+            let response = JSON.stringify({
+                status: "success",
+                "host-ip": true
+            });
+            res.statusCode = 200;
+            res.end(response);
+        });
+
+        blacklist.checkIP("127.0.0.1", "").then((isHostIp) => {
+            expect(isHostIp).toBe(true);
+            done();
+        }).catch((e) => {
+            expect(e).toBeNull();
+            done();
+        });
+    });
+
+    it("should error when the request was not successful", (done) => {
+        mitm.on("request", (req, res) => {
+            let response = JSON.stringify({
+                status: "failure",
+                "host-ip": false
+            });
+            res.statusCode = 200;
+            res.end(response);
+        });
+
+        blacklist.checkIP("127.0.0.1", "").then((isHostIp) => {
+            expect(isHostIp).toBeNull();
+            done();
+        }).catch((e) => {
+            expect(e).toEqual(jasmine.any(Error));
+            done();
+        });
+    });
+
+    it("should error when the request response is malformed", (done) => {
+        mitm.on("request", (req, res) => {
+            res.statusCode = 200;
+            res.end("{asda;g.pdg,");
+        });
+
+        blacklist.checkIP("127.0.0.1", "").then((isHostIp) => {
+            expect(isHostIp).toBeNull();
+            done();
+        }).catch((e) => {
+            expect(e).toEqual(jasmine.any(Error));
+            done();
+        });
+    });
+});
