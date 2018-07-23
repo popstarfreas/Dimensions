0ccd234f83ef2cb4771579fea92d37c2ef13fa8e
Unknown <popstarfreas2@gmail.com>
2018-07-23 22:05:12 +0100

    Use buffer instead of hex strings
    A rudimentary test shows 300% faster for reading and 200% faster for writing (or the other way around). The actual performance bonus might not be as much.

--- spec/dimensions/packets/hexwriterspec.ts
+++ spec/dimensions/packets/hexwriterspec.ts
@@ -5,234 +5,234 @@ describe("hexwriter", () => {
     let hexWriter: HexWriter;
 
     beforeEach(() => {
-        hexWriter = new HexWriter();
+        hexWriter = new HexWriter(900);
     });
 
     it("should start with an empty string of data", () => {
-        expect(hexWriter.data).toEqual("");
+        expect(hexWriter.data.toString("hex")).toEqual("");
     });
 
     describe("strings", () => {
         it("should correctly pack a short string of letters", () => {
             hexWriter.packString("abcdefghijklmnopqrstuvwxyz");
-            expect(hexWriter.data).toEqual("1a6162636465666768696a6b6c6d6e6f707172737475767778797a");
+            expect(hexWriter.data.toString("hex")).toEqual("1a6162636465666768696a6b6c6d6e6f707172737475767778797a");
         });
 
         it("should correctly pack a short string of numbers", () => {
             hexWriter.packString("1234567890");
-            expect(hexWriter.data).toEqual("0a31323334353637383930");
+            expect(hexWriter.data.toString("hex")).toEqual("0a31323334353637383930");
         });
 
         it("should correctly pack a short string of symbols", () => {
             hexWriter.packString("£!\"$%^&*()-='?/>.<,");
-            expect(hexWriter.data).toEqual("14c2a3212224255e262a28292d3d273f2f3e2e3c2c");
+            expect(hexWriter.data.toString("hex")).toEqual("14c2a3212224255e262a28292d3d273f2f3e2e3c2c");
         });
 
         it("should correctly pack multiple short strings", () => {
             hexWriter.packString("abcdefghijklmnopqrstuvwxyz");
             hexWriter.packString("1234567890");
             hexWriter.packString("£!\"$%^&*()-='?/>.<,");
-            expect(hexWriter.data).toEqual("1a6162636465666768696a6b6c6d6e6f707172737475767778797a0a3132333435363738393014c2a3212224255e262a28292d3d273f2f3e2e3c2c");
+            expect(hexWriter.data.toString("hex")).toEqual("1a6162636465666768696a6b6c6d6e6f707172737475767778797a0a3132333435363738393014c2a3212224255e262a28292d3d273f2f3e2e3c2c");
         });
 
         it("should correctly pack a large string", () => {
             hexWriter.packString("abcdefghijklmnopqrstuvwxyz1234567890£!\"$%^&*()-='?/>.<,abcdefghijklmnopqrstuvwxyz1234567890£!\"$%^&*()-='?/>.<,abcdefghijklmnopqrstuvwxyz1234567890£!\"$%^&*()-='?/>.<,abcdefghijklmnopqrstuvwxyz1234567890£!\"$%^&*()-='?/>.<,abcdefghijklmnopqrstuvwxyz1234567890£!\"$%^&*()-='?/>.<,abcdefghijklmnopqrstuvwxyz1234567890£!\"$%^&*()-='?/>.<,abcdefghijklmnopqrstuvwxyz1234567890£!\"$%^&*()-='?/>.<,abcdefghijklmnopqrstuvwxyz1234567890£!\"$%^&*()-='?/>.<,abcdefghijklmnopqrstuvwxyz1234567890£!\"$%^&*()-='?/>.<,abcdefghijklmnopqrstuvwxyz1234567890£!\"$%^&*()-='?/>.<,abcdefghijklmnopqrstuvwxyz1234567890£!\"$%^&*()-='?/>.<,abcdefghijklmnopqrstuvwxyz1234567890£!\"$%^&*()-='?/>.<,");
-            expect(hexWriter.data).toEqual("a0056162636465666768696a6b6c6d6e6f707172737475767778797a31323334353637383930c2a3212224255e262a28292d3d273f2f3e2e3c2c6162636465666768696a6b6c6d6e6f707172737475767778797a31323334353637383930c2a3212224255e262a28292d3d273f2f3e2e3c2c6162636465666768696a6b6c6d6e6f707172737475767778797a31323334353637383930c2a3212224255e262a28292d3d273f2f3e2e3c2c6162636465666768696a6b6c6d6e6f707172737475767778797a31323334353637383930c2a3212224255e262a28292d3d273f2f3e2e3c2c6162636465666768696a6b6c6d6e6f707172737475767778797a31323334353637383930c2a3212224255e262a28292d3d273f2f3e2e3c2c6162636465666768696a6b6c6d6e6f707172737475767778797a31323334353637383930c2a3212224255e262a28292d3d273f2f3e2e3c2c6162636465666768696a6b6c6d6e6f707172737475767778797a31323334353637383930c2a3212224255e262a28292d3d273f2f3e2e3c2c6162636465666768696a6b6c6d6e6f707172737475767778797a31323334353637383930c2a3212224255e262a28292d3d273f2f3e2e3c2c6162636465666768696a6b6c6d6e6f707172737475767778797a31323334353637383930c2a3212224255e262a28292d3d273f2f3e2e3c2c6162636465666768696a6b6c6d6e6f707172737475767778797a31323334353637383930c2a3212224255e262a28292d3d273f2f3e2e3c2c6162636465666768696a6b6c6d6e6f707172737475767778797a31323334353637383930c2a3212224255e262a28292d3d273f2f3e2e3c2c6162636465666768696a6b6c6d6e6f707172737475767778797a31323334353637383930c2a3212224255e262a28292d3d273f2f3e2e3c2c");  
+            expect(hexWriter.data.toString("hex")).toEqual("a0056162636465666768696a6b6c6d6e6f707172737475767778797a31323334353637383930c2a3212224255e262a28292d3d273f2f3e2e3c2c6162636465666768696a6b6c6d6e6f707172737475767778797a31323334353637383930c2a3212224255e262a28292d3d273f2f3e2e3c2c6162636465666768696a6b6c6d6e6f707172737475767778797a31323334353637383930c2a3212224255e262a28292d3d273f2f3e2e3c2c6162636465666768696a6b6c6d6e6f707172737475767778797a31323334353637383930c2a3212224255e262a28292d3d273f2f3e2e3c2c6162636465666768696a6b6c6d6e6f707172737475767778797a31323334353637383930c2a3212224255e262a28292d3d273f2f3e2e3c2c6162636465666768696a6b6c6d6e6f707172737475767778797a31323334353637383930c2a3212224255e262a28292d3d273f2f3e2e3c2c6162636465666768696a6b6c6d6e6f707172737475767778797a31323334353637383930c2a3212224255e262a28292d3d273f2f3e2e3c2c6162636465666768696a6b6c6d6e6f707172737475767778797a31323334353637383930c2a3212224255e262a28292d3d273f2f3e2e3c2c6162636465666768696a6b6c6d6e6f707172737475767778797a31323334353637383930c2a3212224255e262a28292d3d273f2f3e2e3c2c6162636465666768696a6b6c6d6e6f707172737475767778797a31323334353637383930c2a3212224255e262a28292d3d273f2f3e2e3c2c6162636465666768696a6b6c6d6e6f707172737475767778797a31323334353637383930c2a3212224255e262a28292d3d273f2f3e2e3c2c6162636465666768696a6b6c6d6e6f707172737475767778797a31323334353637383930c2a3212224255e262a28292d3d273f2f3e2e3c2c");  
         });
     });
 
     it("should correctly pack network text", () => {
         hexWriter.packNetworkText(new NetworkText(0, "abcdefghijklmnopqrstuvwxyz"));
-        expect(hexWriter.data).toEqual("001a6162636465666768696a6b6c6d6e6f707172737475767778797a");
+        expect(hexWriter.data.toString("hex")).toEqual("001a6162636465666768696a6b6c6d6e6f707172737475767778797a");
     });
 
     it("should correctly pack hex", () => {
         let hex = "a2a2";
         hexWriter.packHex(hex);
-        expect(hexWriter.data).toEqual(hex);
+        expect(hexWriter.data.toString("hex")).toEqual(hex);
     });
 
     describe("bytes", () => {
         it("should correctly pack a byte", () => {
             hexWriter.packByte(50);
-            expect(hexWriter.data).toEqual("32");
+            expect(hexWriter.data.toString("hex")).toEqual("32");
         });
 
         it("should correctly pack a maximum byte", () => {
             hexWriter.packByte(255);
-            expect(hexWriter.data).toEqual("ff");
+            expect(hexWriter.data.toString("hex")).toEqual("ff");
         });
 
         it("should correctly pack a minimum byte", () => {
             hexWriter.packByte(0);
-            expect(hexWriter.data).toEqual("00");
+            expect(hexWriter.data.toString("hex")).toEqual("00");
         });
 
         it("should ignore negativity of numbers", () => {
             hexWriter.packByte(-50);
-            expect(hexWriter.data).toEqual("32");
+            expect(hexWriter.data.toString("hex")).toEqual("32");
         });
     });
 
     describe("colors", () => {
         it("should correctly pack a normal color", () => {
             hexWriter.packColor({R: 255, G: 150, B: 50});
-            expect(hexWriter.data).toEqual("ff9632");
+            expect(hexWriter.data.toString("hex")).toEqual("ff9632");
         });
 
         it("should correctly pack a negative color", () => {
             hexWriter.packColor({R: -255, G: -150, B: -50});
-            expect(hexWriter.data).toEqual("ff9632");
+            expect(hexWriter.data.toString("hex")).toEqual("ff9632");
         });
     });
 
     describe("uint16", () => {
         it("should correctly pack a small uint16", () => {
             hexWriter.packUInt16(50);
-            expect(hexWriter.data).toEqual("3200");
+            expect(hexWriter.data.toString("hex")).toEqual("3200");
         });
 
         it("should correctly pack a maximum uint16", () => {
             hexWriter.packUInt16(65535);
-            expect(hexWriter.data).toEqual("ffff");
+            expect(hexWriter.data.toString("hex")).toEqual("ffff");
         });
 
         it("should correctly pack zero uint16", () => {
             hexWriter.packUInt16(0);
-            expect(hexWriter.data).toEqual("0000");
+            expect(hexWriter.data.toString("hex")).toEqual("0000");
         });
     });
 
     describe("int16", () => {
         it("should correctly pack a small int16", () => {
             hexWriter.packInt16(50);
-            expect(hexWriter.data).toEqual("3200");
+            expect(hexWriter.data.toString("hex")).toEqual("3200");
         });
 
         it("should correctly pack maximum int16", () => {
             hexWriter.packInt16(32767);
-            expect(hexWriter.data).toEqual("ff7f");
+            expect(hexWriter.data.toString("hex")).toEqual("ff7f");
         });
 
         it("should correctly pack zero int16", () => {
             hexWriter.packInt16(0);
-            expect(hexWriter.data).toEqual("0000");
+            expect(hexWriter.data.toString("hex")).toEqual("0000");
         });
 
         it("should correctly pack negative int16", () => {
             hexWriter.packInt16(-255);
-            expect(hexWriter.data).toEqual("01ff");
+            expect(hexWriter.data.toString("hex")).toEqual("01ff");
         });
 
         it("should correctly pack largest negative int16", () => {
             hexWriter.packInt16(-32768);
-            expect(hexWriter.data).toEqual("0080");
+            expect(hexWriter.data.toString("hex")).toEqual("0080");
         });
     });
 
     describe("uint32", () => {
         it("should correctly pack a small uint32", () => {
             hexWriter.packUInt32(50);
-            expect(hexWriter.data).toEqual("32000000");
+            expect(hexWriter.data.toString("hex")).toEqual("32000000");
         });
 
         it("should correctly pack a medium uint32", () => {
             hexWriter.packUInt32(32767);
-            expect(hexWriter.data).toEqual("ff7f0000");
+            expect(hexWriter.data.toString("hex")).toEqual("ff7f0000");
         });
 
         it("should correctly pack maximum uint32", () => {
             hexWriter.packUInt32(4294967295);
-            expect(hexWriter.data).toEqual("ffffffff");
+            expect(hexWriter.data.toString("hex")).toEqual("ffffffff");
         });
 
         it("should correctly pack zero uint32", () => {
             hexWriter.packUInt32(0);
-            expect(hexWriter.data).toEqual("00000000");
+            expect(hexWriter.data.toString("hex")).toEqual("00000000");
         });
     });
 
     describe("int32", () => {
         it("should correctly pack a small int32", () => {
             hexWriter.packInt32(50);
-            expect(hexWriter.data).toEqual("32000000");
+            expect(hexWriter.data.toString("hex")).toEqual("32000000");
         });
 
         it("should correctly pack a medium int32", () => {
             hexWriter.packInt32(32767);
-            expect(hexWriter.data).toEqual("ff7f0000");
+            expect(hexWriter.data.toString("hex")).toEqual("ff7f0000");
         });
 
         it("should correctly pack maximum int32", () => {
             hexWriter.packInt32(2147483647);
-            expect(hexWriter.data).toEqual("ffffff7f");
+            expect(hexWriter.data.toString("hex")).toEqual("ffffff7f");
         });
 
         it("should correctly pack zero int32", () => {
             hexWriter.packInt32(0);
-            expect(hexWriter.data).toEqual("00000000");
+            expect(hexWriter.data.toString("hex")).toEqual("00000000");
         });
 
         it("should correctly pack negative int32", () => {
             hexWriter.packInt32(-255);
-            expect(hexWriter.data).toEqual("01ffffff");
+            expect(hexWriter.data.toString("hex")).toEqual("01ffffff");
         });
 
         it("should correctly pack a large negative int32", () => {
             hexWriter.packInt32(-2147483648);
-            expect(hexWriter.data).toEqual("00000080");
+            expect(hexWriter.data.toString("hex")).toEqual("00000080");
         });
     });
 
     describe("single", () => {
         it("should correctly pack a small integer", () => {
             hexWriter.packSingle(50);
-            expect(hexWriter.data).toEqual("00004842");
+            expect(hexWriter.data.toString("hex")).toEqual("00004842");
         });
 
         it("should correctly pack a medium integer", () => {
             hexWriter.packSingle(32767);
-            expect(hexWriter.data).toEqual("00feff46");
+            expect(hexWriter.data.toString("hex")).toEqual("00feff46");
         });
 
         it("should correctly pack a large integer", () => {
             hexWriter.packSingle(2147483647);
-            expect(hexWriter.data).toEqual("0000004f");
+            expect(hexWriter.data.toString("hex")).toEqual("0000004f");
         });
 
         it("should correctly pack zero integer", () => {
             hexWriter.packSingle(0);
-            expect(hexWriter.data).toEqual("00000000");
+            expect(hexWriter.data.toString("hex")).toEqual("00000000");
         });
 
         it("should correctly pack negative integer", () => {
             hexWriter.packSingle(-255);
-            expect(hexWriter.data).toEqual("00007fc3");
+            expect(hexWriter.data.toString("hex")).toEqual("00007fc3");
         });
 
         it("should correctly pack a large negative integer", () => {
             hexWriter.packSingle(-2147483648);
-            expect(hexWriter.data).toEqual("000000cf");
+            expect(hexWriter.data.toString("hex")).toEqual("000000cf");
         });
 
         it("should correctly pack a small single", () => {
             hexWriter.packSingle(0.5);
-            expect(hexWriter.data).toEqual("0000003f");
+            expect(hexWriter.data.toString("hex")).toEqual("0000003f");
         });
 
         it("should correctly pack a small negative single", () => {
             hexWriter.packSingle(-0.5);
-            expect(hexWriter.data).toEqual("000000bf");
+            expect(hexWriter.data.toString("hex")).toEqual("000000bf");
         });
 
         it("should correctly pack a large single", () => {
             hexWriter.packSingle(2147483647.5);
-            expect(hexWriter.data).toEqual("0000004f");
+            expect(hexWriter.data.toString("hex")).toEqual("0000004f");
         });
 
         it("should correctly pack a large negative single", () => {
             hexWriter.packSingle(-2147483647.5);
-            expect(hexWriter.data).toEqual("000000cf");
+            expect(hexWriter.data.toString("hex")).toEqual("000000cf");
         });
     });
 });
--- spec/dimensions/packets/packetwriterspec.ts
+++ spec/dimensions/packets/packetwriterspec.ts
@@ -1,74 +1,75 @@
 import PacketWriter from 'dimensions/packets/packetwriter';
 
 describe("packetwriter", () => {
-    let packetWriter: PacketWriter;
-
-    beforeEach(() => {
-        packetWriter = new PacketWriter();
-    });
-
     it("should correctly write the type of packet", () => {
+        const packetWriter: PacketWriter = new PacketWriter(3);
         packetWriter.setType(5);
-        expect(packetWriter.data.substr(4)).toEqual("05");
+        expect(packetWriter.data.readInt8(2)).toEqual(5);
     });
 
     describe("length", () => {
-        it("should correctly start with the right length", () => {
-            expect(packetWriter.data).toEqual("0200");
-        });
-
         it("should correctly set the length of the packet after setting type", () => {
+            const packetWriter: PacketWriter = new PacketWriter(3);
             packetWriter.setType(5);
-            expect(packetWriter.data.substr(0, 4)).toEqual("0300");
+            expect(packetWriter.data.readInt16LE(0)).toEqual(3);
         });
 
         it("should correctly set the length of the packet after packing a byte", () => {
+            const packetWriter: PacketWriter = new PacketWriter(4);
             packetWriter.setType(5);
             packetWriter.packByte(1);
-            expect(packetWriter.data.substr(0, 4)).toEqual("0400");
+            console.log(packetWriter.buf);
+            expect(packetWriter.data.readInt16LE(0)).toEqual(4);
         });
 
         it("should correctly set the length of the packet after packing a uint16", () => {
+            const packetWriter: PacketWriter = new PacketWriter(5);
             packetWriter.setType(5);
             packetWriter.packUInt16(1);
-            expect(packetWriter.data.substr(0, 4)).toEqual("0500");
+            expect(packetWriter.data.readInt16LE(0)).toEqual(5);
         });
 
         it("should correctly set the length of the packet after packing an int16", () => {
+            const packetWriter: PacketWriter = new PacketWriter(5);
             packetWriter.setType(5);
             packetWriter.packInt16(1);
-            expect(packetWriter.data.substr(0, 4)).toEqual("0500");
+            expect(packetWriter.data.readInt16LE(0)).toEqual(5);
         });
 
         it("should correctly set the length of the packet after packing a uint32", () => {
+            const packetWriter: PacketWriter = new PacketWriter(7);
             packetWriter.setType(5);
             packetWriter.packUInt32(1);
-            expect(packetWriter.data.substr(0, 4)).toEqual("0700");
+            expect(packetWriter.data.readInt16LE(0)).toEqual(7);
         });
 
         it("should correctly set the length of the packet after packing an int32", () => {
+            const packetWriter: PacketWriter = new PacketWriter(7);
             packetWriter.setType(5);
             packetWriter.packInt32(1);
-            expect(packetWriter.data.substr(0, 4)).toEqual("0700");
+            expect(packetWriter.data.readInt16LE(0)).toEqual(7);
         });
 
         it("should correctly set the length of the packet after packing a single", () => {
+            const packetWriter: PacketWriter = new PacketWriter(7);
             packetWriter.setType(5);
             packetWriter.packSingle(5);
-            expect(packetWriter.data.substr(0, 4)).toEqual("0700");
+            expect(packetWriter.data.readInt16LE(0)).toEqual(7);
         });
 
         it("should correctly set the length of the packet after packing a short string", () => {
+            const packetWriter: PacketWriter = new PacketWriter(9);
             packetWriter.setType(5);
             packetWriter.packString("1 3 5");
-            expect(packetWriter.data.substr(0, 4)).toEqual("0900");
+            expect(packetWriter.data.readInt16LE(0)).toEqual(9);
         });
 
         it("should correctly set the length of the packet after packing a large string", () => {
+            const packetWriter: PacketWriter = new PacketWriter(677);
             let str = "abcdefghijklmnopqrstuvwxyz1234567890£!\"$%^&*()-='?/>.<,abcdefghijklmnopqrstuvwxyz1234567890£!\"$%^&*()-='?/>.<,abcdefghijklmnopqrstuvwxyz1234567890£!\"$%^&*()-='?/>.<,abcdefghijklmnopqrstuvwxyz1234567890£!\"$%^&*()-='?/>.<,abcdefghijklmnopqrstuvwxyz1234567890£!\"$%^&*()-='?/>.<,abcdefghijklmnopqrstuvwxyz1234567890£!\"$%^&*()-='?/>.<,abcdefghijklmnopqrstuvwxyz1234567890£!\"$%^&*()-='?/>.<,abcdefghijklmnopqrstuvwxyz1234567890£!\"$%^&*()-='?/>.<,abcdefghijklmnopqrstuvwxyz1234567890£!\"$%^&*()-='?/>.<,abcdefghijklmnopqrstuvwxyz1234567890£!\"$%^&*()-='?/>.<,abcdefghijklmnopqrstuvwxyz1234567890£!\"$%^&*()-='?/>.<,abcdefghijklmnopqrstuvwxyz1234567890£!\"$%^&*()-='?/>.<,";
             packetWriter.setType(5);
             packetWriter.packString(str);
-            expect(packetWriter.data.substr(0, 4)).toEqual("a502");
+            expect(packetWriter.data.readInt16LE(0)).toEqual(677);
         });
     });
 });
