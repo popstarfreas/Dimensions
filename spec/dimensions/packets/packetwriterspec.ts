9f1676c4529ac51647d2d7fd5af8978156603cd2
Unknown <popstarfreas2@gmail.com>
2018-07-23 22:26:47 +0100

    Remove need to specify length
    All method calls on the writer are deferred and the size incremented for each one prior, until .data or .buf is called which is when all the data is written.

--- spec/dimensions/packets/packetwriterspec.ts
+++ spec/dimensions/packets/packetwriterspec.ts
@@ -2,70 +2,69 @@ import PacketWriter from 'dimensions/packets/packetwriter';
 
 describe("packetwriter", () => {
     it("should correctly write the type of packet", () => {
-        const packetWriter: PacketWriter = new PacketWriter(3);
+        const packetWriter: PacketWriter = new PacketWriter();
         packetWriter.setType(5);
         expect(packetWriter.data.readInt8(2)).toEqual(5);
     });
 
     describe("length", () => {
         it("should correctly set the length of the packet after setting type", () => {
-            const packetWriter: PacketWriter = new PacketWriter(3);
+            const packetWriter: PacketWriter = new PacketWriter();
             packetWriter.setType(5);
             expect(packetWriter.data.readInt16LE(0)).toEqual(3);
         });
 
         it("should correctly set the length of the packet after packing a byte", () => {
-            const packetWriter: PacketWriter = new PacketWriter(4);
+            const packetWriter: PacketWriter = new PacketWriter();
             packetWriter.setType(5);
             packetWriter.packByte(1);
-            console.log(packetWriter.buf);
             expect(packetWriter.data.readInt16LE(0)).toEqual(4);
         });
 
         it("should correctly set the length of the packet after packing a uint16", () => {
-            const packetWriter: PacketWriter = new PacketWriter(5);
+            const packetWriter: PacketWriter = new PacketWriter();
             packetWriter.setType(5);
             packetWriter.packUInt16(1);
             expect(packetWriter.data.readInt16LE(0)).toEqual(5);
         });
 
         it("should correctly set the length of the packet after packing an int16", () => {
-            const packetWriter: PacketWriter = new PacketWriter(5);
+            const packetWriter: PacketWriter = new PacketWriter();
             packetWriter.setType(5);
             packetWriter.packInt16(1);
             expect(packetWriter.data.readInt16LE(0)).toEqual(5);
         });
 
         it("should correctly set the length of the packet after packing a uint32", () => {
-            const packetWriter: PacketWriter = new PacketWriter(7);
+            const packetWriter: PacketWriter = new PacketWriter();
             packetWriter.setType(5);
             packetWriter.packUInt32(1);
             expect(packetWriter.data.readInt16LE(0)).toEqual(7);
         });
 
         it("should correctly set the length of the packet after packing an int32", () => {
-            const packetWriter: PacketWriter = new PacketWriter(7);
+            const packetWriter: PacketWriter = new PacketWriter();
             packetWriter.setType(5);
             packetWriter.packInt32(1);
             expect(packetWriter.data.readInt16LE(0)).toEqual(7);
         });
 
         it("should correctly set the length of the packet after packing a single", () => {
-            const packetWriter: PacketWriter = new PacketWriter(7);
+            const packetWriter: PacketWriter = new PacketWriter();
             packetWriter.setType(5);
             packetWriter.packSingle(5);
             expect(packetWriter.data.readInt16LE(0)).toEqual(7);
         });
 
         it("should correctly set the length of the packet after packing a short string", () => {
-            const packetWriter: PacketWriter = new PacketWriter(9);
+            const packetWriter: PacketWriter = new PacketWriter();
             packetWriter.setType(5);
             packetWriter.packString("1 3 5");
             expect(packetWriter.data.readInt16LE(0)).toEqual(9);
         });
 
         it("should correctly set the length of the packet after packing a large string", () => {
-            const packetWriter: PacketWriter = new PacketWriter(677);
+            const packetWriter: PacketWriter = new PacketWriter();
             let str = "abcdefghijklmnopqrstuvwxyz1234567890£!\"$%^&*()-='?/>.<,abcdefghijklmnopqrstuvwxyz1234567890£!\"$%^&*()-='?/>.<,abcdefghijklmnopqrstuvwxyz1234567890£!\"$%^&*()-='?/>.<,abcdefghijklmnopqrstuvwxyz1234567890£!\"$%^&*()-='?/>.<,abcdefghijklmnopqrstuvwxyz1234567890£!\"$%^&*()-='?/>.<,abcdefghijklmnopqrstuvwxyz1234567890£!\"$%^&*()-='?/>.<,abcdefghijklmnopqrstuvwxyz1234567890£!\"$%^&*()-='?/>.<,abcdefghijklmnopqrstuvwxyz1234567890£!\"$%^&*()-='?/>.<,abcdefghijklmnopqrstuvwxyz1234567890£!\"$%^&*()-='?/>.<,abcdefghijklmnopqrstuvwxyz1234567890£!\"$%^&*()-='?/>.<,abcdefghijklmnopqrstuvwxyz1234567890£!\"$%^&*()-='?/>.<,abcdefghijklmnopqrstuvwxyz1234567890£!\"$%^&*()-='?/>.<,";
             packetWriter.setType(5);
             packetWriter.packString(str);
